13ac0ab16424340685c826b3a8f4fdc53380b3bb
Matt T <code.matthew.thompson@gmail.com>
2016-12-02 11:01:33 -0500

    Gets search results working

--- client/src/components/dashboard/dashboard.jsx
+++ client/src/components/dashboard/dashboard.jsx
@@ -17,10 +17,10 @@ class Dashboard extends Component {
     )
   }
 
-  renderFavorites (favorites, component) {
+  renderFavorites (favorites) {
     if (favorites.length > 0) {
       return favorites.map((favorite, index) => (
-        <FavoriteCard key={index} favorite={favorite} favorites={favorites} parent={component} />
+        <FavoriteCard favorite={favorite} index={index} />
       ))
     } else {
       return []
@@ -29,51 +29,45 @@ class Dashboard extends Component {
 
   render () {
     return (
-      <div className='App App-header'>
-        <div className='mdl-tooltip' data-mdl-for='tt2'>
-          Everything you see is inside a container named Dashboard.
-          It knows about Redux and has props and actions
-          connected to it! You can find it in /redux/containers/dashboard.js.
-        </div>
-        <div id='tt2'>
-          <div className='App-logo'>
-            <img src={logo} className='App-logo' alt='logo' />
+      <div className='container'>
+        <div className='App App-header'>
+          <div className='row'>
+            <div className='mdl-tooltip' data-mdl-for='tt2'>
+              Everything you see is inside a container named Dashboard.
+              It knows about Redux and has props and actions
+              connected to it! You can find it in /redux/containers/dashboard.js.
+            </div>
+            <div id='tt2'>
+              <div className='App-logo'>
+                <img src={logo} className='App-logo' alt='logo' />
+              </div>
+              <h6>Welcome to Redux + React + Rails boilerplate</h6>
+              <div className='subtitle'><span className='highlight'>React</span> provided via create-react-app package <i className='fa fa-heart highlight fa-1x' aria-hidden='true' /></div>
+            </div>
           </div>
-          <h6>Welcome to Redux + React + Rails boilerplate</h6>
-          <div className='subtitle'><span className='highlight'>React</span> provided via create-react-app package <i className='fa fa-heart highlight fa-1x' aria-hidden='true' /></div>
+          <div className='row'>
+            <VisibleLoginForm />
+          </div>
+          <br />
+          <div className='row'>
+            <SearchForm
+              valueChangeCB={this.handleValueChange.bind(this)}
+              searchCB={this.handleSearch.bind(this)} />
+          </div>
+          {this.props.favorites.length > 0
+            ? <div className='row'>
+              <div className='results'>
+                <ul className='demo-list-control mdl-list'>
+                  { this.renderFavorites(this.props.favorites) }
+                </ul>
+              </div>
+            </div>
+            : <div className='searchErr'><strong>No search results found, try again...</strong></div>
+          }
         </div>
-        <VisibleLoginForm />
-        <br />
-        <SearchForm
-          valueChangeCB={this.handleValueChange.bind(this)}
-          searchCB={this.handleSearch.bind(this)} />
-        {this.props.favorites.length > 0
-          ? this.renderFavorites(this.props.favorites, this)
-          : <div className='searchErr'><strong>No search results found, try again...</strong></div>
-        }
       </div>
     )
   }
 }
-
-// const Job = ({job, parent, jobs}) => {
-//   return (
-//     <div id={job.id} className='card text-md-center jobresult'
-//       onMouseOver={() => parent.props._uiActions.changeLocation({
-//         lat: job.lat,
-//         lng: job.lng
-//       })}>
-//       <div className='card-block'>
-//         <h4 className='card-title'>{job.title}</h4>
-//         <h6 className='card-subtitle text-muted'>{job.address}</h6>
-//       </div>
-//       <img className='searchresult-img' src={'https://builditreact.s3.amazonaws.com/uploads/user/avatar/' + job.user_id + '/image.png'} alt='Card image' />
-//       <div className='card-block'>
-//         <p className='card-text'>{job.description}</p>
-//         <p><button onClick={() => parent.props._uiActions.showJob(job.id, jobs)} className='btn btn-primary'>{job.hourly_rate / 100 + '$/hr '}Details</button></p>
-//       </div>
-//     </div>
-//   )
-// }
 
 export default Dashboard
--- client/src/components/favorites/favorite-card.jsx
+++ client/src/components/favorites/favorite-card.jsx
@@ -5,7 +5,15 @@ class FavoriteCard extends Component {
   render () {
     return (
       <div>
-        favorite-card container component
+        <li className='mdl-list__item'>
+          <span className='resultbox mdl-list__item-primary-content'>
+            <div className='img-fluid'>
+              <img className='favicon' src={'http://icons.duckduckgo.com/ip2/' + this.props.favorite.url + '/.ico'} alt='icon' />
+            </div>
+            <i className='fa fa-link fx-1x' aria-hidden='true' />
+            {this.props.favorite.name} - <a href={'http://' + this.props.favorite.url}>{this.props.favorite.url}</a>
+          </span>
+        </li>
       </div>
     )
   }
