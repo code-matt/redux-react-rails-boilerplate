725e006ed9704bf062e4f52b1390d538b9e2c1fc
Matt T <code.matthew.thompson@gmail.com>
2016-12-03 16:31:22 -0500

    Makes educational tooltips react-mdl components and they work again

--- client/src/components/dashboard/dashboard.jsx
+++ client/src/components/dashboard/dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import './dashboard.css'
-// import './react-treeview.css'
+import Tooltip from 'react-mdl/lib/Tooltip'
 import logo from './logo.svg'
 
 import VisibleLoginForm from '../../redux/containers/login'
@@ -47,19 +47,16 @@ class Dashboard extends Component {
           <div className='row'>
             <div className='col-md-7'>
               <div className='row'>
-                <div className='mdl-tooltip' data-mdl-for='tt2'>
-                  Everything you see is inside a container named Dashboard.
-                  It knows about Redux and has props and actions
-                  connected to it! You can find it in /redux/containers/dashboard.js.
-                </div>
-                <div id='tt2'>
-                  <div className='App-logo'>
-                    <img src={logo} className='App-logo' alt='logo' />
+                <Tooltip position='left' label='Everything you see is inside a container named Dashboard. It knows about Redux and has props and actions connected to it! You can find it in /redux/containers/dashboard.js.'>
+                  <div id='tt2'>
+                    <div className='App-logo'>
+                      <img src={logo} className='App-logo' alt='logo' />
+                    </div>
+                    <h6>Welcome to Redux + React + Rails boilerplate</h6>
+                    <div className='subtitle'><span className='highlight'>React</span> provided via create-react-app package <i className='fa fa-heart highlight fa-1x' aria-hidden='true' /></div>
+                    <div className='subtitle'>Make sure to fetch the <span className='highlight'><a href='https://github.com/code-matt/redux-react-rails-boilerplate/tree/clean_version' target='_blank'>clean version</a></span> if you are going to use it!</div>
                   </div>
-                  <h6>Welcome to Redux + React + Rails boilerplate</h6>
-                  <div className='subtitle'><span className='highlight'>React</span> provided via create-react-app package <i className='fa fa-heart highlight fa-1x' aria-hidden='true' /></div>
-                  <div className='subtitle'>Make sure to fetch the <span className='highlight'><a href='https://github.com/code-matt/redux-react-rails-boilerplate/tree/clean_version' target='_blank'>clean version</a></span> if you are going to use it!</div>
-                </div>
+                </Tooltip>
               </div>
               <div className='row'>
                 {this.props.token
